7239a6a13c58f0ffab4943b3a6c4bcb7e9660e8a
Firestar04O <ricardo.espino.b@tecsup.edu.pe>
2025-09-17 17:45:13 -0500

    lane and ball, pins, player input, physics base implemented

---
+++ Assets/Scripts/ScoringSystem.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ScoringSystem : MonoBehaviour
+{
+    [SerializeField] int points;
+    [SerializeField] int currentPoints;
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
